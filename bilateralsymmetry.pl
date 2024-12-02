8296bc27eeee5b2ac6af819854286134b0adeaf6
Yuk Yin Fung <163593025+Aron3888@users.noreply.github.com>
2024-12-02 05:31:46 +0000

    Update bilateralsymmetry.pl

--- bilateralsymmetry.pl
+++ bilateralsymmetry.pl
@@ -13,27 +13,28 @@ number_loop([X|XS], N)
      N is W*10 + X.
 
 % Generate valid partitions of digits
-generator3([X, XS]) :-
-    permutation([1,2,3,4,5,6,7,8,9], Digits),
-    append(X, XS, Digits),
-    X \= [], XS \= [].
+generator3([X, XS]) 
+  :- permutation([1,2,3,4,5,6,7,8,9], Digits),
+     append(X, XS, Digits),
+     X \= [], XS \= [].
 
 % Select partitions meeting criteria
-selector3([M, N]) :-
-    number(M, MS),
-    number(N, NS),
-    Pr is MS*NS,
-    number_chars(Pr, Z),
-    reverse(Z, Z),
-    Pr >= 40000000,
-    Pr < 50000000,
-    Min is min(MS, NS),
-    Min mod 10 =:= 3,
-    Sum is MS + NS + 100,
-    number_chars(Sum, Y),
-    reverse(Y, Y).
+selector3([M, N]) 
+  :- number(M, MS),
+     number(N, NS),
+     Pr is MS*NS,
+     number_chars(Pr, Z),
+     reverse(Z, Z),
+     Pr >= 40000000,
+     Pr < 50000000,
+     Min is min(MS, NS),
+     Min mod 10 =:= 3,
+     Sum is MS + NS + 100,
+     number_chars(Sum, Y),
+     reverse(Y, Y).
 
 % Reverses a list
-reverse([X|XS], R) :-
-    reverse(XS, W),
-    append(W, [X], R).
+reverse([], []).
+reverse([X|XS], R) 
+  :- reverse(XS, W),
+     append(W, [X], R).
