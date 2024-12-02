0d73d13cca3c3b8e866633b66751af972ce9a4de
Yuk Yin Fung <163593025+Aron3888@users.noreply.github.com>
2024-12-02 05:30:26 +0000

    Create testnumber.pl

---
+++ tests/testnumber.pl
@@ -0,0 +1,11 @@
+% 
+
+x_warmup3(N5) :-
+  x_unit(number([] , 0) , 0 , N1 ) ,
+  x_unit(number([9] , 9) , N1 , N2 ) ,
+  x_unit(number([4 ,7] , 47) , N2 , N3 ) ,
+  x_unit(number([2 ,5 ,4] , 254) , N3 , N4 ) ,
+  x_unit(number([9 ,8 ,7 ,6] , 9876) , N4 , N5 ) .
+
+x_unit(F , M , N ):- 
+    call( F ) -> N is M + 1; N is M.
