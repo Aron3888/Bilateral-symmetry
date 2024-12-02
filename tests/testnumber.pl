915e6b2d8ff99d9a67325a909d7ca1e54b11e344
Yuk Yin Fung <163593025+Aron3888@users.noreply.github.com>
2024-12-02 05:42:37 +0000

    Update testnumber.pl

--- tests/testnumber.pl
+++ tests/testnumber.pl
@@ -1,11 +1,10 @@
-% 
+% tests the lists of digits correspond to its respective numeric value
+x_warmup3(N5) :- 
+  x_unit(number([], 0), 0, N1) ,
+  x_unit(number([9], 9), N1, N2) ,
+  x_unit(number([4 ,7], 47), N2, N3) ,
+  x_unit(number([2 ,5 ,4], 254), N3, N4) ,
+  x_unit(number([9 ,8 ,7 ,6], 9876), N4, N5) .
 
-x_warmup3(N5) :-
-  x_unit(number([] , 0) , 0 , N1 ) ,
-  x_unit(number([9] , 9) , N1 , N2 ) ,
-  x_unit(number([4 ,7] , 47) , N2 , N3 ) ,
-  x_unit(number([2 ,5 ,4] , 254) , N3 , N4 ) ,
-  x_unit(number([9 ,8 ,7 ,6] , 9876) , N4 , N5 ) .
-
-x_unit(F , M , N ):- 
-    call( F ) -> N is M + 1; N is M.
+x_unit(F, M, N) :- 
+    call(F) -> N is M + 1; N is M.
